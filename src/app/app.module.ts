58c60b5514416cc938a8e79359f09f9100b7555c
homo-digitalis <secretary@homo-digitalis.org>
2019-06-17 15:56:59 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MondayComponent } from './monday/monday.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TuesdayComponent } from './tuesday/tuesday.component';
+import { WednesdayComponent } from './wednesday/wednesday.component';
+import { ThursdayComponent } from './thursday/thursday.component';
+import { FridayComponent } from './friday/friday.component';
+import { SaturdayComponent } from './saturday/saturday.component';
+import { SundayComponent } from './sunday/sunday.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MondayComponent,
+    LandingPageComponent,
+    TuesdayComponent,
+    WednesdayComponent,
+    ThursdayComponent,
+    FridayComponent,
+    SaturdayComponent,
+    SundayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
